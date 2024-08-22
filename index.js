6d0f76dfc870ddd24582e32faff1b450cea734e6
kareemAlqubati <salamkareem792@gmail.com>
2024-08-22 15:27:30 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function customerName(coustomerName) {
+
+  return customerName;
+}
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = "Bob";
+function changeLeastFavoriteCustomer(newCustomer) {
+  leastFavoriteCustomer = newCustomer;
+}
+changeLeastFavoriteCustomer("maybe bob");
